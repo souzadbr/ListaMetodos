afd95d6bce8471bf39be3e9f0a3a29080aed09da
Debora Souza <deboral.p@hotmail.com>
2021-09-22 10:31:07 -0300

    Exercicio 3 encapsulamentos Pessoa ajustado no métodos idadeAtual

--- src/br/com/zup/EncapsulamentoExercicio3/Main.java
+++ src/br/com/zup/EncapsulamentoExercicio3/Main.java
@@ -2,9 +2,9 @@ package br.com.zup.EncapsulamentoExercicio3;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa humano = new Pessoa("Debora", 1992, 1.65);
-        humano.identidade();
-        System.out.println(humano.anosCompletos(2021));
+        Pessoa humano = new Pessoa("Debora", 1992, 1.65, 0);
+        humano.mostrarDados();
+        humano.idadeAtual(2021);
 
 
 
--- src/br/com/zup/EncapsulamentoExercicio3/Pessoa.java
+++ src/br/com/zup/EncapsulamentoExercicio3/Pessoa.java
@@ -4,17 +4,21 @@ public class Pessoa {
     private String nome;
     private int dataNascimento;
     private double altura;
+    private int idade;
 
-    public Pessoa(String nome, int dataNascimento, double altura) {
+    public Pessoa(String nome, int dataNascimento, double altura, int idade) {
         this.altura = altura;
         this.dataNascimento = dataNascimento;
         this.nome = nome;
+        this.idade = idade;
     }
 
     public String getNome() {
         return nome;
     }
-
+    public int getIdade(){
+        return idade;
+    }
     public int getDataNascimento() {
         return dataNascimento;
     }
@@ -26,6 +30,9 @@ public class Pessoa {
     public void setNome(String nome) {
         this.nome = nome;
     }
+    public void setIdade(int idade){
+        this.idade= idade;
+    }
 
     public void setDataNascimento(int dataNascimento) {
         this.dataNascimento = dataNascimento;
@@ -35,17 +42,17 @@ public class Pessoa {
         this.altura = altura;
     }
 
-    public void identidade() {
+    public void mostrarDados() {
         System.out.println("Nome da pessoa é: " + getNome());
         System.out.println("Altura da pessoa é " + getAltura());
         System.out.println("A data de nascimento é: " + getDataNascimento());
+
     }
 
 
-    public int anosCompletos(int anoAtual) {
-      int  idade = anoAtual - getDataNascimento();
-        System.out.println("A idade da pessoa é "+ idade);
-        return idade;
+    public void idadeAtual (int anoAtual){
+        setIdade(anoAtual-getDataNascimento());
+        System.out.println("A idade da pessoa é: "+ getIdade());
     }
 
 
